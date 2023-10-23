960a8b9527ef28d725d2f6dbb9653c8fcf4a73d1
SvetonosovS <tris23@bk.ru>
2023-10-23 23:03:53 +0300

    taskHomework4

---
+++ Lesson4/User.java
@@ -0,0 +1,30 @@
+package Lesson4;
+
+public class User {
+
+    String firstName;
+    String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User() {};
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
